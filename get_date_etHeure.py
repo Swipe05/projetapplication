6b124019d9bbd2a9808c4b13ae446929b957d9d5
TTSiWOOD <nguyencongkhai2k1vl@gmail.com>
2022-06-21 11:57:19 +0200

    get number match of a ...

--- get_date_etHeure.py
+++ get_date_etHeure.py
@@ -1,3 +1,5 @@
+from lib2to3.refactor import get_all_fix_names
+from sqlite3 import Timestamp
 import interact_with_db
 import AMERGEKAI
 from datetime import datetime
@@ -9,19 +11,29 @@ class HeureEtDate:
         self.database = interact_with_db.Data_base("LienMinh.db")
 
     def timestamp_to_date(self, timestamp):
-        dmy = datetime.fromtimestamp(timestamp).strftime('%d/%m/%y')
+        dmy = datetime.fromtimestamp(timestamp).strftime('%m/%d/%y')
         DMY = dmy.split("/")
         return DMY
     # format:  "day/month/year"
-    def date_to_timestamp(self, DMY):
-        return time.mktime(datetime.strptime(DMY, "%d/%m/%Y").timetuple())
+    def date_to_timestamp(self, MDY):
+        return time.mktime(datetime.strptime(MDY, "%m/%d/%Y").timetuple())
 
     # select match_start_time, match_end_time, number_of_match_in_a_day, timestamp
     # datemonthyear format is %d/%m/%y
-    def select_data(self, daymonthyear):
-        return self.database.select_data_with_condition(self.pseudo,f" date LIKE   '{daymonthyear}%'",('timestamp','date'))
+    def get_data_of_a_day(self, monthdayyear):
+        return self.database.select_data_with_condition(self.pseudo,f" date LIKE   '{monthdayyear}%'",('timestamp','date'))
+
+    def get_all_data_a_month(self, month, year):
+        return self.database.select_data_with_condition(self.pseudo,f" date LIKE   '%{month}%{year}%'",('timestamp','date'))
+
+    def get_number_of_match_a_month(self, month, year):
+        return  len(self.get_all_data_a_month(month,year))
+
+    def get_number_of_match_a_day(self, month , day ,  year):
+        rq = f"{month}/{day}/{year}"
+        return len(self.get_data_of_a_day(rq))
 
 
 
-get_swipe = HeureEtDate("TTSiHades")
-get_swipe.select_data("04/03/2022")
+get_swipe = HeureEtDate("HentEye")
+print(get_swipe.get_number_of_match_a_day("04","04","2022"))
